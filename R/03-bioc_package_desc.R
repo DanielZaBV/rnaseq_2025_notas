afc499795733ba7d5edd926efc1d1e8bcc33ca26
Daniel Zaid Bautista Vargas <danielzb@lcg.unam.mx>
2025-02-09 13:33:26 -0600

    Actividad de bioconductor

---
+++ R/03-bioc_package_desc.R
@@ -0,0 +1,17 @@
+# Para instalar el paquete:
+BiocManager::install("GEOquery")
+
+# Descripcion del paquete:
+# GEOquery
+# Bioconductor version: Release (3.20)
+# The NCBI Gene Expression Omnibus (GEO) is a public repository of microarray data.
+# Given the rich and varied nature of this resource, it is only natural to want to apply BioConductor tools to these data.
+# GEOquery is the bridge between GEO and BioConductor.
+# Author: Sean Davis [aut, cre]
+# Maintainer: Sean Davis <seandavi at gmail.com>
+# Citation (from within R, enter citation("GEOquery")):
+# Davis S, Meltzer P (2007). “GEOquery: a bridge between the Gene Expression Omnibus (GEO) and BioConductor.”
+# Bioinformatics, 14, 1846–1847. doi:10.1093/bioinformatics/btm254.
+# URL: https://bioconductor.org/packages/release/bioc/html/GEOquery.html
+# Documentation: https://bioconductor.org/packages/release/bioc/vignettes/GEOquery/inst/doc/GEOquery.html
+# Dependencias: 78
